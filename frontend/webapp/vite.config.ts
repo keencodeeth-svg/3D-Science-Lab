7a049dc61b6ac9ea2a9260f895af2f41f88afcfe
keencodeeth-svg <keencode.eth@gmail.com>
2026-03-13 09:34:46 +0800

    Fix three.js chunk splitting

--- frontend/webapp/vite.config.ts
+++ frontend/webapp/vite.config.ts
@@ -26,7 +26,13 @@ export default defineConfig({
           const normalizedId = id.replace(/\\/g, '/');
 
           if (normalizedId.indexOf('node_modules/three/examples') !== -1) return 'three-extras';
-          if (normalizedId.indexOf('node_modules/three/src/renderers') !== -1 || normalizedId.indexOf('node_modules/three/src/textures') !== -1) return 'three-rendering';
+          if (
+            normalizedId.indexOf('node_modules/three/src/math/') !== -1 ||
+            normalizedId.lastIndexOf('/node_modules/three/src/constants.js') === normalizedId.length - '/node_modules/three/src/constants.js'.length ||
+            normalizedId.lastIndexOf('/node_modules/three/src/utils.js') === normalizedId.length - '/node_modules/three/src/utils.js'.length
+          ) {
+            return 'three-foundation';
+          }
           if (normalizedId.indexOf('node_modules/three/src') !== -1) return 'three-vendor';
           if (normalizedId.indexOf('node_modules/react') !== -1 || normalizedId.indexOf('node_modules/scheduler') !== -1) return 'react-vendor';
           return undefined;
